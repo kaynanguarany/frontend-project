422a1fcaf6bb12554b720e4f939b11e670654174
kayna guarany <kaynanguarany@gmail.com>
2020-06-27 14:27:54 -0300

    Add ngnix config annd dockerfile dev

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,8 @@ COPY . .
 RUN npm run build
 
 FROM nginx
-EXPOSE 80
 COPY --from=0 /app/build /usr/share/nginx/html
+RUN rm /etc/nginx/conf.d/default.conf
+COPY nginx/nginx.conf /etc/nginx/conf.d
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Card, Table } from 'react-bootstrap';
+import { Container, Spinner, Table } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 
@@ -11,9 +11,50 @@ class Board extends Component {
 
   componentDidMount = () => this.props.fetchList();
 
-  render() {
-    const { list } = this.props;
+  renderContent = () => {
+    const { loading, list } = this.props;
 
+    if (loading) {
+      return (
+        <div className='text-center'>
+          <Spinner animation="border" variant="primary" />
+        </div>
+      )
+    }
+
+    return (
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Shortened URL</th>
+            <th>URL</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          { list.map((item, idx) => {
+            return (
+              <tr key={idx}>
+                <td>
+                  <a href={item.url} target='_blank' rel='noopener noreferrer'>{item.url}</a>
+                </td>
+                <td>
+                  <a href={item.destination_url} target='_blank' rel='noopener noreferrer'>
+                    {item.destination_url}
+                  </a>
+                </td>
+                <td>{item.access_count}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
+    )
+  }
+
+  render() {
+   
     return (
       <div>
         <Header />
@@ -21,35 +62,7 @@ class Board extends Component {
           <div className='text-center board-title'>
             <h2>100 most frequently accessed URLs</h2>
           </div>
-        
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Shortened URL</th>
-                    <th>URL</th>
-                    <th>Quantity</th>
-                  </tr>
-                </thead>
-
-                <tbody>
-                  { list.map((item, idx) => {
-                    return (
-                      <tr key={idx}>
-                        <td>
-                          <a href={item.url} target='_blank' rel='noopener noreferrer'>{item.url}</a>
-                        </td>
-                        <td>
-                          <a href={item.destination_url} target='_blank' rel='noopener noreferrer'>
-                            {item.destination_url}
-                          </a>
-                        </td>
-                        <td>{item.access_count}</td>
-                      </tr>
-                    )
-                  })}
-                </tbody>
-              </Table>
-           
+          {this.renderContent()}
         </Container>
       </div>
     );
